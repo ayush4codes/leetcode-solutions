52339837973eada5c0faad11f65188d651284790
Ayush Mishra <amb97955@gmail.com>
2026-06-25 12:15:10 +0530

    Sync solution for LeetCode 860 - Lemonade Change

--- LeetCode/problems/0860-lemonade-change/solution.cpp
+++ LeetCode/problems/0860-lemonade-change/solution.cpp
@@ -3,27 +3,26 @@ public:
     bool lemonadeChange(vector<int>& bills) {
         int five_c = 0;
         int ten_c = 0;
-        int n = bills.size();
-        for (int i = 0; i < n; i++) {
-            if (bills[i] == 5) {
+        for (int i : bills){
+            if (i == 5) {
                 five_c++;
             } 
-            else if (bills[i] == 10) {
-                if (five_c == 0) {
+            else if (i == 10){
+                if (five_c == 0){
                     return false;
                 }
                 five_c--;
                 ten_c++;
             } 
-            else if (bills[i] == 20) {
-                if (ten_c > 0 && five_c > 0) {
+            else if (i == 20){
+                if (ten_c > 0 && five_c > 0){
                     ten_c--;
                     five_c--;
                 } 
-                else if (five_c >= 3) {
+                else if (five_c >= 3){
                     five_c -= 3;
                 } 
-                else {
+                else{
                     return false;
                 }
             }
