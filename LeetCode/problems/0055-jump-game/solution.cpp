d588edf8322e1e69bf1621e9aa268246699f6983
Ayush Mishra <amb97955@gmail.com>
2026-06-25 12:15:01 +0530

    Sync solution for LeetCode 55 - Jump Game

--- LeetCode/problems/0055-jump-game/solution.cpp
+++ LeetCode/problems/0055-jump-game/solution.cpp
@@ -1,23 +1,18 @@
 class Solution {
-    vector<int> dp;
-    int n;
 public:
     bool canJump(vector<int>& nums) {
-        n = nums.size();
-        dp.resize(n,-1);
-        return find(nums,0);
-    }
-    bool find(vector<int>& nums,int i) {
-        if (i == nums.size()-1) return true;
-        if (dp[i] != -1) return dp[i];
-        for (int j = i+1; j<= min(n-1,i+nums[i]);j++) {
-            bool check = find(nums,j);
-            if (check) {
-                dp[i] = 1;
+        int n=nums.size();
+        int reach=0;
+        
+        for(int i=0;i<n;i++){
+            if (i>reach){
+                return false;
+            }
+            reach=max(reach,i+nums[i]);
+
+            if (reach>=n-1){
                 return true;
             }
-        }
-        dp[i] = 0;
-        return false;
+        }return true;
     }
 };
