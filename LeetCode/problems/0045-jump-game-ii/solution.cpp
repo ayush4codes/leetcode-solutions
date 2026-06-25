3ff357bc32b3834988ad57b7980d72f246df3fe4
Ayush Mishra <amb97955@gmail.com>
2026-06-25 12:14:53 +0530

    Sync solution for LeetCode 45 - Jump Game II

--- LeetCode/problems/0045-jump-game-ii/solution.cpp
+++ LeetCode/problems/0045-jump-game-ii/solution.cpp
@@ -1,14 +1,20 @@
 class Solution {
+    vector<int> dp;
+    int n;
 public:
     int jump(vector<int>& nums) {
-        int maxi = 0, curr = 0, count = 0;
-        for (int i  =0;i<nums.size()-1;i++) {
-            maxi = max(maxi,i+nums[i]);
-            if (i ==  curr) {
-                count++;
-                curr = maxi;
-            }
+        n = nums.size();
+        dp.resize(n,-1);
+        return find(nums,0);
+    }
+    int find(vector<int>& nums,int i) {
+        if (i == n-1) return 0;
+        if (dp[i] != -1) return dp[i];
+        int mini = INT_MAX/2;
+        for (int j = i+1;j<= min(n-1,i+nums[i]);j++) {
+            int curr = find(nums,j);
+            mini = min(curr+1,mini);
         }
-        return count;
+        return  dp[i] = mini;
     }
 };
